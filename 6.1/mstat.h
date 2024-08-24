ea91818cbc491e328bb41b612550449e07c9b9b8
Sofico <sophiasemenova28@gmail.com>
2024-08-25 03:02:01 +0700

    новый файл:    6.1/Makefile 	новый файл:    6.1/main.cpp 	новый файл:    6.1/mstat.cpp 	новый файл:    6.1/mstat.h

---
+++ 6.1/mstat.h
@@ -0,0 +1,46 @@
+#ifndef MSTAT_H
+#define MSTAT_H
+
+#define L_NAME 10
+#define L_NUMBER 15
+#define ETC 20
+
+typedef struct {
+    char firstName[L_NAME];
+    char secondName[L_NAME];
+    char lastName[L_NAME];
+} Name;
+
+typedef struct {
+    bool flag;
+    char phoneNumberMore[L_NUMBER];
+    char jobPlace[ETC];
+    char jobTitle[ETC];
+    char eMailAddressMain[ETC];
+    char eMailAddressMore[ETC];
+    char linkNetwork[ETC];
+} Info;
+
+struct Contact {
+    int id;
+    Name name;
+    char phoneNumberMain[L_NUMBER];
+    Info info;
+    struct Contact* prev;
+    struct Contact* next;
+};
+
+struct PhoneBook {
+    struct Contact* head;
+    struct Contact* tail;
+    int n;
+};
+
+void SetContact(struct Contact* contact);
+void AddContact(struct PhoneBook* phoneBook);
+void PrintContacts(struct PhoneBook* phoneBook);
+void DeleteContact(struct PhoneBook* phoneBook, int index);
+void menu();
+void freePhoneBook(struct PhoneBook* phoneBook);
+
+#endif // MSTAT_H
